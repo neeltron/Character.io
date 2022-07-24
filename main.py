0c12667211a4ac3c1b8f23f73de326112eb16f5e
Neel Adwani <nprosofficial@gmail.com>
2022-07-24 07:05:49 +0000

    added spacebar refresh functionality

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template
+import random
+
 app = Flask('app', template_folder = "templates", static_folder = "gen_data")
 
 @app.route('/')
 def characterio():
-  return render_template('index.html')
+  rando = random.randint(1, 250)
+  return render_template('index.html', data = rando)
 
 app.run(host='0.0.0.0', port=8080)
